c62eeeb7fdc436b94544ead5b63e9bd126d97614
Gonçalo Henriques <goncalo.minuscode@gmail.com>
2015-01-02 13:57:56 +0000

    Initial Commit

---
+++ jogabo_friend_picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jogabo_friend_picker
+//
+//  Created by Gonçalo Henriques on 02/01/15.
+//  Copyright (c) 2015 Mobigeek Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
